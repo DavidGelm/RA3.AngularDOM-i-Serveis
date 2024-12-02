92be65896c5e5afba8d699c50fbdee76ed1f163b
DavidGelm <david.gelma@institutvidreres.cat>
2024-12-02 12:06:11 +0100

    Exercici 1 Dom i Serveis

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
+  standalone: true,
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ExercicisAngularDomiServeisDavidGelmaDam1';
+  constructor(){}
+  ngOnInit(){
+    const element = document.getElementById('text');
+    element!.innerText = element!.innerText.toUpperCase();
+  }
 }
